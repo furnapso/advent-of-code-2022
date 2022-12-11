948435a8ebd5346808f0bc67d8741aaa98643cc5
furnapso <luke.boland95@gmail.com>
2022-12-11 10:16:44 +1000

    day 1

---
+++ 01.py
@@ -0,0 +1,24 @@
+def main():
+    with open("01_input.txt") as f:
+        input = f.read()
+
+    elves = input.split("\n\n")
+    elf_totals = []
+
+    for elf in elves:
+        elf_totals.append(get_reindeer_total(elf))
+
+    print(f"Part one: {max(elf_totals)}")
+
+    sorted_elf_totals = sorted(elf_totals, reverse=True)
+    print(f"Part two: {sum(sorted_elf_totals[:3])}")
+
+
+def get_reindeer_total(elf: list) -> int:
+    vals = filter(lambda x: x != '', elf.split("\n"))
+    vals = map(lambda x: int(x), vals)
+    return sum(vals)
+
+
+if __name__ == "__main__":
+    main()
