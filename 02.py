b6612862cedd6e34c7a242e70f1a5a5a616ecdee
furnapso <luke.boland95@gmail.com>
2022-12-11 22:29:23 +1000

    simplify conditional statements

--- 02.py
+++ 02.py
@@ -48,18 +48,14 @@ def calculate_score(player1: str, player2: str) -> int:
 
     if player2_decoded == 'ROCK' and player1_decoded == 'SCISSORS':
         score += 6
-        return score
     elif player2_decoded == 'PAPER' and player1_decoded == 'ROCK':
         score += 6
-        return score
     elif player2_decoded == 'SCISSORS' and player1_decoded == 'PAPER':
         score += 6
-        return score
     elif player2_decoded == player1_decoded:
         score += 3
-        return score
-    else:
-        return score
+
+    return score
 
 
 def get_move(player1: str, outcome: str):
